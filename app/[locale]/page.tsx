41166d1c99bb60d8b0fbd34f335545ce0a43a99c
Andriy Romanov <mgm.agro04@gmail.com>
2025-07-24 16:27:11 +0300

    edit bug

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,13 @@
 import PostList from "@/components/PostList";
 import { Post } from "@/types/post";
 
-export default async function Home({ params }: { params: { locale: string } }) {
+export default async function Home({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   if (!res.ok) throw new Error("Failed to fetch posts");
 
@@ -10,7 +16,7 @@ export default async function Home({ params }: { params: { locale: string } }) {
   return (
     <div>
       <h1>Posts</h1>
-      <PostList posts={posts} locale={params.locale} />
+      <PostList posts={posts} locale={locale} />
     </div>
   );
 }
